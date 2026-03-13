7b02e79f4820e30dcbf6b1d7b566f6f1e3c162c9
Arthur <arthuralvesgab@gmail.com>
2026-03-13 18:18:17 -0300

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,36 @@
+function calcularRank(vitorias, derrotas) {
+    
+    let saldoVitorias = vitorias - derrotas
+    let nivel = ""
+
+    //rank
+    if (vitorias <= 10) {
+        nivel = "ferro"
+
+    } else if (vitorias <= 20) {
+        nivel = "bronze"
+        
+    } else if (vitorias <= 50) {
+        nivel = "prata"
+        
+    } else if (vitorias <= 50) {
+        nivel = "ouro"
+        
+    } else if (vitorias <= 90) {
+        nivel = "diamente"
+        
+    } else if (vitorias <= 100) {
+        nivel = "lendario"
+        
+    } else {
+        nivel ="imortal" 
+    }
+    return{saldoVitorias,nivel}
+}
+
+//resultado
+let vitorias = 75
+let derrotas = 20
+
+let resultado = calcularRank(vitorias, derrotas)
+console.log("O heroi tem saldo de" + resultado.saldoVitorias + "está no nível de" + resultado.nivel )
